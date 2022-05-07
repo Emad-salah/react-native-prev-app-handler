159d4c87e166ab25e6746083a20ed1c7fdddeaf6
Emad-salah <emads14@gmail.com>
2022-05-07 18:15:43 +0000

    Minor fixes

--- ios/PrevAppHandler.m
+++ ios/PrevAppHandler.m
@@ -4,27 +4,27 @@
 @import UIKit;
 @import ObjectiveC.runtime;
 
-inline BOOL jumpBackToPreviousApp() {
-    Ivar sysNavIvar = class_getInstanceVariable(UIApplication.class, "_systemNavigationAction");
-    UIApplication* app = UIApplication.sharedApplication;
-    UISystemNavigationAction* action = object_getIvar(app, sysNavIvar);
-    if (!action) {
-        return NO;
-    }
-    NSUInteger destination = action.destinations.firstObject.unsignedIntegerValue;
-    return [action sendResponseForDestination:destination];
-}
-
+@interface UISystemNavigationAction : NSObject
+@property(nonatomic, readonly, nonnull) NSArray<NSNumber*>* destinations;
+-(BOOL)sendResponseForDestination:(NSUInteger)destination;
+@end
 
 @implementation PrevAppHandler
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(minimize:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(minimize:(RCTPromiseResolveBlock)resolve:(RCTPromiseRejectBlock)reject)
 {
-    // TODO: Implement some actually useful functionality
-    NSBool response = [BOOL jumpBackToPreviousApp];
-    callback(response);
+    Ivar sysNavIvar = class_getInstanceVariable(UIApplication.class, "_systemNavigationAction");
+    UIApplication* app = UIApplication.sharedApplication;
+    UISystemNavigationAction* action = object_getIvar(app, sysNavIvar);
+    if (!action) {
+        reject(@"error", @"Navigation error", nil);
+        return;
+    }
+    NSUInteger destination = action.destinations.firstObject.unsignedIntegerValue;
+    [action sendResponseForDestination:destination];
+    resolve(@"Navigation completed successfully!");
 }
 
 @end
